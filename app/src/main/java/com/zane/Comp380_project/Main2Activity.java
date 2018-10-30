fb838d86fbd1ac5070c9188e99304aab966b24ea
SilverDiscoPanda <rudy.robles.178@my.csun.edu>
2018-10-29 18:41:01 -0700

    date on calendar click

--- app/src/main/java/com/zane/Comp380_project/Main2Activity.java
+++ app/src/main/java/com/zane/Comp380_project/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.zane.Comp380_project;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.CalendarView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //receives intent and displays date
+        Intent getevent = getIntent();
+        String date = getevent.getStringExtra("date");
+        TextView theDate = (TextView) findViewById(R.id.date);
+        theDate.setText(date);
+
     }
 }
--- app/src/main/java/com/zane/Comp380_project/MainActivity.java
+++ app/src/main/java/com/zane/Comp380_project/MainActivity.java
@@ -5,29 +5,41 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private  static final String TAG = "CalendarActivity";
+    //  private TextView theDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CalendarView calendarView = findViewById(R.id.calendarView);
+
+
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-                @Override
-                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                    // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
-                    Log.e("abc","123");
-                    String date = "Selected date is " + (month + 1) + "/" + dayOfMonth + "/" + year;
-                     Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
-                    Intent event = new Intent(MainActivity.this, Main2Activity.class);
-                    startActivity(event);
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+                // Log.e("abc","123");
 
-                    //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
 
-                }
-            });
+                String date = (month + 1) + "/" + dayOfMonth + "/" + year;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+
+                //Goes to Main2Activity
+                Intent event = new Intent(MainActivity.this, Main2Activity.class);
+                event.putExtra("date",date);
+
+                startActivity(event);
+
+
+            }
+        });
     }
 }
+
 
 
